ecffd17a6ece490c4f82beabafbbdfb569f474ef
sebasdelalv340 <sdelgado@iesrafaelalberti.es>
2024-02-22 16:27:30 +0100

    Fun saludar/contarChiste

--- src/Persona.kt
+++ src/Persona.kt
@@ -1,8 +1,14 @@
 abstract class Persona(val nombre: String, var edad: Int, var humor: Boolean) {
 
+    fun saludar(): String = "Hola gente"
 
-
-
-
+    fun contarChiste(): String {
+        if (humor) {
+            return "-Papá, papá, quiero ir al circo.\n" +
+                    "-¿Qué dices niño? Quién quiera verte que venga a casa."
+        } else {
+            return "No estoy de humor para contar chistes."
+        }
+    }
 
 }
