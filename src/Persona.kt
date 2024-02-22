8bba16f795fb680979b308c4c62dcd7ecabfa6e8
aaracan246 <acalberto@iesrafaelalberti.es>
2024-02-22 17:06:30 +0100

    Creando clases

--- src/Persona.kt
+++ src/Persona.kt
@@ -1,8 +1,39 @@
-abstract class Persona(val nombre: String, var edad: Int, var humor: Boolean) {
+enum class TIPO_ROPA{
+
+    PAYASO, OFICINISTA, UNIFORME_POLICIA, UNIFORME_BOMBERO
+}
+
+enum class ARMA{
+
+    PEZ_COMICAMENTE_GRANDE, MALETIN, PISTOLA, MANGUERA
+}
+
+
+abstract class Persona(val nombre: String, var edad: Int, var humor: Boolean = true) {
+
+
+    fun enfadarse(){
+
+        humor = !humor
+
+    }
+}
+
+class Payaso(nombre: String, edad: Int, humor: Boolean, tipoDeRopa: TIPO_ROPA, arma: ARMA): Persona(nombre, edad, humor){
 
 
 
 
+}
 
+class Oficinista(nombre: String, edad: Int, humor: Boolean, tipoDeRopa: TIPO_ROPA, arma: ARMA): Persona(nombre, edad, humor){
+
+}
+
+class Policia(nombre: String, edad: Int, humor: Boolean, tipoDeRopa: TIPO_ROPA, arma: ARMA): Persona(nombre, edad, humor){
+
+}
+
+class Bombero(nombre: String, edad: Int, humor: Boolean, tipoDeRopa: TIPO_ROPA, arma: ARMA): Persona(nombre, edad, humor){
 
 }
